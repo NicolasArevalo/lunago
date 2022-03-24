27b8ffaa476efe67c24d2d3887cc382f591dc44b
Nicolás Arévalo <inag.458@gmail.com>
2022-03-23 19:47:06 -0500

    Fixing the loading color to dark theme

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import './Loading.scss'
 
 import loguito from './../../assets/NAicon-01.svg'
+import loguitoBlanco from './../../assets/NAiconWhite.svg'
 
 const Loading = () => {
+  const body = document.getElementById('body')
+  const darkClassExist = body.classList[1]
   return (
     <div className='bg'>
-        <img src={loguito} alt="logo" className="logo" />
+        {
+            darkClassExist !== undefined
+              ? <img src={loguitoBlanco} alt="logo" className="logo" />
+              : <img src={loguito} alt="logo" className="logo" />
+          }
     </div>
   )
 }
